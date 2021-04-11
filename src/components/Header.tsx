38fd4f6e8626f879499a73e2cef3dd1e7059de90
jaf88 <jaf.alves8@gmail.com>
2021-04-11 08:50:26 -0300

    feat: Dashboard page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
-import React from 'react';
 import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
