f3c9da9eff894976d664d1d85096146e067434a2
jaf88 <jaf.alves8@gmail.com>
2021-04-19 15:14:47 -0300

    feat: Components

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,15 @@
+import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
+
+export function Profile() {
+  return (
+    <Flex align="center">
+      <Box mr="4" textAling="right">
+        <Text>Joaquim Alves</Text>
+        <Text color="gray.300" fontSize="small">
+          jaf.alves8@gmail.com
+            </Text>
+      </Box>
+      <Avatar size="md" name="Joaquim Alves" src="https://github.com/jaf88.png" />
+    </Flex>
+  );
+}
