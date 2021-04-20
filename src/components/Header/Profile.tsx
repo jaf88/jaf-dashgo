70300779c3908f3bfd6bb816cfc194bdccf0256e
jaf88 <jaf.alves8@gmail.com>
2021-04-20 10:34:05 -0300

    feat: responsive sidebar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAling="right">
-        <Text>Joaquim Alves</Text>
-        <Text color="gray.300" fontSize="small">
-          jaf.alves8@gmail.com
-            </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAling="right">
+          <Text>Joaquim Alves</Text>
+          <Text color="gray.300" fontSize="small">
+            jaf.alves8@gmail.com
+          </Text>
+        </Box>
+      ) }
       <Avatar size="md" name="Joaquim Alves" src="https://github.com/jaf88.png" />
     </Flex>
   );
